f96a4c608485246ab3dbbfdec12a756cf626ed36
Arno Smit <appel268576@gmail.com>
2015-07-03 08:54:01 +0200

    missing var name for dataModel

--- FoodTracker/DataController.swift
+++ FoodTracker/DataController.swift
@@ -69,6 +69,15 @@ class DataController {
                         let entityDescription = NSEntityDescription.entityForName("USDAItem", inManagedObjectContext: managedObjectContext!)
                         let usdaItem = USDAItem(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
                         usdaItem.idValue = itemDictionary["_id"]! as! String
+                        usdaItem.dateAdded = NSDate()
+                        
+                        if itemDictionary["fields"] != nil {
+                            let fieldsDictionary = itemDictionary["fields"]! as! NSDictionary
+                            if fieldsDictionary["item_name"] != nil {
+                                usdaItem.name = fieldsDictionary["item_name"]! as! String
+                                
+                            }
+                        }
                     }
                 }
                 
